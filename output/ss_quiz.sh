006e3dc3c30059e34d823224dabe208eb8b33611
marcusgreen <marcusavgreen@gmail.com>
2023-01-01 08:18:39 +0000

    tweaks to ss_quiz.sh

--- output/ss_quiz.sh
+++ output/ss_quiz.sh
@@ -1,1 +1,1 @@
-java -jar schemaspy-6.1.0.jar -desc='Quiz tables'   -i "(quiz|question||question_usages|question_attempts|quiz_grades|quiz_attempts|quiz_sections|quiz_slots)" -o moodle_401/quiz
+java -jar schemaspy-6.1.0.jar -desc='Quiz tables: got to http://www.examulator.com/er for all Moodle tables' -imageformat=svg  -i "(quiz|question||question_usages|question_attempts|quiz_grades|quiz_attempts|quiz_sections|quiz_slots|quiz_overrides|quiz_feedback)" -o moodle_401/quiz -template=./quiztemplates/
