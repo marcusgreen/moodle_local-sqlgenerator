61e9543bbae197e5a978cb4c0e001822fece474b
marcusgreen <marcusavgreen@gmail.com>
2023-02-09 09:15:13 +0000

    add imageformat to be svg for much smaller output

--- output/ss.sh
+++ output/ss.sh
@@ -1,1 +1,1 @@
-java -jar schemaspy-6.1.0.jar -meta=meta.xml -desc='Moodle 3.8 database schema with additional foreign key relationships see http://www.examulator.com/er' -template=./templates/
+java -jar schemaspy-6.1.0.jar -imageformat=svg  -meta=meta.xml -desc='Moodle 3.8 database schema with additional foreign key relationships see http://www.examulator.com/er' -template=./templates/
