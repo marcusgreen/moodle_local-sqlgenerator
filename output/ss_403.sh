7cba84366f5a8114b9259802899bc4d8f7ce44bf
Marcus Green <marcusavgreen@gmail.com>
2024-04-06 20:10:05 +0100

    Create ss_403.sh

---
+++ output/ss_403.sh
@@ -0,0 +1,1 @@
+java -jar schemaspy-6.1.0.jar -v -imageformat=svg -meta=meta.xml -desc='Moodle 4.3 database schema with additional foreign key relationships see http://www.examulator.com/er' -template=./templates/
